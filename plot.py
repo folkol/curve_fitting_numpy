b4a8473eebcf7c150bf6283935e6cbaa1b0f667e
Mattias Johansson <mattias.johansson@atex.com>
2014-09-02 15:38:37 +0200

    Volatile writes

--- plot.py
+++ plot.py
@@ -1,24 +1,35 @@
 import numpy as np
-from scipy.optimize import curve_fit
-from scipy.stats.distributions import t
-
-x=np.array([1, 10, 100, 1000, 2000])
-
-cy=np.array([20, 188, 318, 1096, 816])
-my=np.array([24, 45, 55, 91, 92])
-title='Contents: 10.000 x 10.000kb, Aspects: 0 x 0kb'
-filename='write2.png'
-
+import scipy as sp
+from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
-plt.plot(x,cy,'bo')
-plt.plot(x,cy,'b-', label='Couch')
-plt.plot(x,my,'ro')
-plt.plot(x,my,'r-', label='MySQL')
+
+x=np.array(range(1, 20, 2))
+y = [
+2640.2604712004218,
+6401.311398258293,
+8679.857098304676,
+10114.110428082759,
+10673.308595450224,
+11629.644662022174,
+11843.275565782882,
+11879.86933569315,
+12537.850202530162,
+11615.470319685619]
+
+title='Volatile contents: 100.000 x 0kb, Aspects: 0 x 0kb'
+
+new_length = 25
+new_x = np.linspace(x.min(), x.max(), new_length)
+new_y = sp.interpolate.interp1d(x, y, kind='cubic')(new_x)
+
+plt.plot(new_x, new_y, label='Volatile writes')
+plt.plot(x, y, 'o')
 plt.legend(loc='best')
 plt.xlabel('Number of threads', fontsize=10)
 plt.ylabel('Writes per sec', fontsize=10)
-plt.axis([-100, 2100, -100, cy.max() + 100])
-plt.title(title)
+plt.axis([0, 20, 0, max(y) + 1000])
 plt.grid()
-#plt.show()
-plt.savefig('images/' + filename)
+plt.title(title)
+plt.show()
+
+
