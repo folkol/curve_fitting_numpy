46b024a7c98a5b9df83e31a84c6063325f782026
Mattias Johansson <mattias.johansson@atex.com>
2014-09-03 09:27:58 +0200

    Some more refactoring

--- plot.py
+++ plot.py
@@ -1,30 +1,30 @@
 import numpy as np
-import scipy as sp
-from scipy.interpolate import interp1d
+from scipy import interpolate
 import matplotlib.pyplot as plt
 import sys
 
 
-def doStuff(line):
-    y = [int(elem) for elem in line.split()]
+def plot_figure(counter, line):
+    y = [float(elem) for elem in line.split()]
     x=np.array(range(1, 20, 2))
 
     title='Volatile contents: 100.000 x 0kb, Aspects: 0 x 0kb'
     
     new_length = 25
     new_x = np.linspace(x.min(), x.max(), new_length)
-    new_y = sp.interpolate.interp1d(x, y, kind='cubic')(new_x)
+    new_y = interpolate.interp1d(x, y, kind='cubic')(new_x)
     
     plt.plot(new_x, new_y, label='Volatile writes')
-    plt.plot(x, y, 'o')
+    plt.plot(x, y, 'or')
     plt.legend(loc='best')
     plt.xlabel('Number of threads', fontsize=10)
     plt.ylabel('Writes per sec', fontsize=10)
-    plt.axis([0, 20, 0, max(y) + 1000])
+    plt.axis([0, 20, 0, 1.1*max(y)])
     plt.grid()
     plt.title(title)
-#   plt.show()
-    plt.savefig('test.png')
+    plt.savefig('test%d.png' % counter)
+    plt.close()
 
-for line in sys.stdin:
-    doStuff(line)
+
+for (counter, line) in enumerate(sys.stdin):
+    plot_figure(counter, line)
