4e205857b6e114edc5351a57cb70a375732d8c19
Mattias Johansson <mattias.johansson@atex.com>
2014-09-03 12:13:15 +0200

    Less hard coded parameters

--- plot.py
+++ plot.py
@@ -7,18 +7,18 @@ from sys import stdin
 title = stdin.readline()
 x = [float(elem) for elem in stdin.readline().split()]
 
-lines = [map(float, row.split()) for row in stdin]
+lines = [map(float, row.split()) for row in stdin if row.strip()]
 y = np.average(lines, axis=0)
 
-interp_x = np.linspace(min(x), max(x), 25)
-interp_y = interpolate.interp1d(x, y, kind='cubic')(interp_x)
+ix = np.linspace(min(x), max(x), 25)
+iy = interpolate.interp1d(x, y, kind='cubic')(ix)
 
 plt.xlabel('Number of threads', fontsize=10)
 plt.ylabel('Writes per sec', fontsize=10)
-plt.axis([0, 2000, min(interp_y) - 0.1*max(interp_y), 1.1*max(interp_y)])
+plt.axis([0, 1.1*max(x), min(iy) - 0.1*max(iy), 1.1*max(iy)])
 plt.grid()
 plt.title(title)
-plt.plot(interp_x, interp_y, '-b', label='Writes per second')
+plt.plot(ix, iy, '-b', label='Writes per second')
 plt.plot(x, y, 'or')
 plt.legend(loc='upper left')
 plt.savefig('plot.png')
