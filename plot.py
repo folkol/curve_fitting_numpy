9dd589029420df1f5de8a3f25ede272ef169b346
Mattias Johansson <mattias.johansson@atex.com>
2014-09-03 09:38:48 +0200

    Simplified the script. It now takes a title and a line of numbers.

--- plot.py
+++ plot.py
@@ -1,30 +1,23 @@
 import numpy as np
 from scipy import interpolate
 import matplotlib.pyplot as plt
-import sys
+from sys import stdin
 
 
-def plot_figure(counter, line):
-    y = [float(elem) for elem in line.split()]
-    x=np.array(range(1, 20, 2))
+title = stdin.readline()
+x = np.array(range(1, 20, 2))
+y = [float(elem) for elem in stdin.readline().split()]
 
-    title='Volatile contents: 100.000 x 0kb, Aspects: 0 x 0kb'
-    
-    new_length = 25
-    new_x = np.linspace(x.min(), x.max(), new_length)
-    new_y = interpolate.interp1d(x, y, kind='cubic')(new_x)
-    
-    plt.plot(new_x, new_y, label='Volatile writes')
-    plt.plot(x, y, 'or')
-    plt.legend(loc='best')
-    plt.xlabel('Number of threads', fontsize=10)
-    plt.ylabel('Writes per sec', fontsize=10)
-    plt.axis([0, 20, min(y) - 0.1*max(y), 1.1*max(y)])
-    plt.grid()
-    plt.title(title)
-    plt.savefig('plot_%d.png' % counter)
-    plt.close()
+new_x = np.linspace(x.min(), x.max(), 25)
+new_y = interpolate.interp1d(x, y, kind='cubic')(new_x)
 
-
-for (counter, line) in enumerate(sys.stdin):
-    plot_figure(counter, line)
+plt.plot(new_x, new_y, '-b', label='Writes per second')
+plt.plot(x, y, 'or')
+plt.legend(loc='upper left')
+plt.xlabel('Number of threads', fontsize=10)
+plt.ylabel('Writes per sec', fontsize=10)
+plt.axis([0, 20, min(new_y) - 0.1*max(new_y), 1.1*max(new_y)])
+plt.grid()
+plt.title(title)
+plt.savefig('plot.png')
+plt.close()
